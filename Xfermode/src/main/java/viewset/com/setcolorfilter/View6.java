951e525cc2925569d223792f1ac30941cfa39dc9
chao <zhouzechao10@163.com>
2018-12-25 17:00:57 +0800

    切割图片

--- Xfermode/src/main/java/viewset/com/setcolorfilter/View6.java
+++ Xfermode/src/main/java/viewset/com/setcolorfilter/View6.java
@@ -1,93 +1,79 @@
-//package viewset.com.setcolorfilter;
-//
-//import android.animation.ValueAnimator;
-//import android.content.Context;
-//import android.graphics.Bitmap;
-//import android.graphics.BitmapFactory;
-//import android.graphics.Canvas;
-//import android.graphics.Color;
-//import android.graphics.Paint;
-//import android.graphics.Path;
-//import android.graphics.PorterDuff;
-//import android.graphics.PorterDuffXfermode;
-//import android.support.annotation.Nullable;
-//import android.util.AttributeSet;
-//import android.view.View;
-//import android.view.animation.LinearInterpolator;
-//
-//public class View6 extends View {
-//
-//    private final int width;
-//    private final int height;
-//    private final Bitmap dstB;
-//    Paint paint;
-//    Paint paintText;
-//    Bitmap srcB;
-//    private int dx;
-//
-//    public View6(Context context) {
-//        this(context, null);
-//    }
-//
-//    public View6(Context context, @Nullable AttributeSet attrs) {
-//        this(context, attrs, 0);
-//    }
-//
-//    public View6(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-//        super(context, attrs, defStyleAttr);
-//        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-//
-//        paint = new Paint();
-//        paint.setColor(Color.WHITE);
-//        paint.setAntiAlias(true);
-//        paint.setStrokeCap(Paint.Cap.ROUND);
-//        paint.setStyle(Paint.Style.FILL_AND_STROKE);
-//        paint.setStrokeWidth(2);
-//
-//        dstB = BitmapFactory.decodeResource(getResources(), R.mipmap.bolang, null);
-//
-//        width = 500;
-//        height = 500;
-//        srcB = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-//    }
-//
-//    /**
-//     * 圆角图片，重点是
-//     * Canvas c = new Canvas(dstB); 把bitmap的dstB传进画布重新构图
-//     * c.drawPath(path, paint);
-//     *
-//     * @param canvas
-//     */
-//    @Override
-//    protected void onDraw(Canvas canvas) {
-//        super.onDraw(canvas);
-//
-//
-//        Canvas c = new Canvas(srcB);
-//
-//
-//        canvas.drawColor(Color.BLACK);
-//        canvas.drawBitmap(textB, 0, 0, paint);
-//        int layerId = canvas.saveLayer(0, 0, getWidth(), getHeight(), null, Canvas.ALL_SAVE_FLAG);
-//        canvas.drawBitmap(dstB, 0, 0, paint);
-//        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
-//        canvas.drawBitmap(textB, 0, 0, paint);
-//        paint.setXfermode(null);
-//        canvas.restoreToCount(layerId);
-//    }
-//
-//    public void startAnim() {
-//        ValueAnimator animator = ValueAnimator.ofInt(0, waveLen);
-//        animator.setDuration(2000);
-//        animator.setRepeatCount(ValueAnimator.INFINITE);
-//        animator.setInterpolator(new LinearInterpolator());
-//        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-//            @Override
-//            public void onAnimationUpdate(ValueAnimator animation) {
-//                dx = (int) animation.getAnimatedValue();
-//                postInvalidate();
-//            }
-//        });
-//        animator.start();
-//    }
-//}
+package viewset.com.setcolorfilter;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+
+public class View6 extends View {
+
+    private final int width;
+    private final int height;
+    private final Bitmap dstB;
+    Paint paint;
+    Paint paintText;
+    Bitmap srcB;
+    private int dx;
+
+    public View6(Context context) {
+        this(context, null);
+    }
+
+    public View6(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public View6(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+
+        paint = new Paint();
+        paint.setColor(Color.WHITE);
+        paint.setAntiAlias(true);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        paint.setStrokeWidth(2);
+
+        dstB = BitmapFactory.decodeResource(getResources(), R.mipmap.bolang, null);
+
+        width = 500;
+        height = 500;
+        srcB = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+    }
+
+    /**
+     * 圆角图片，重点是
+     * Canvas c = new Canvas(dstB); 把bitmap的dstB传进画布重新构图
+     * c.drawPath(path, paint);
+     *
+     * @param canvas
+     */
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int layerId = canvas.saveLayer(0, 0, getWidth(), getHeight(), null, Canvas.ALL_SAVE_FLAG);
+        canvas.restoreToCount(layerId);
+    }
+
+    public void startAnim() {
+        ValueAnimator animator = ValueAnimator.ofInt(0, dstB.getWidth());
+        animator.setDuration(2000);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                dx = (int) animation.getAnimatedValue();
+                postInvalidate();
+            }
+        });
+        animator.start();
+    }
+}
